57a366571e38c5d3e1372b48b52f5e5214435a23
SGrottel <sgrottel@sgrottel.de>
2025-06-04 19:00:13 +0200

    Cloning all defines from "lua.h" into "versioninfo.h"
    because clang rc cannot handle std headers included from lua.h (any more)

--- numericVersionNumber.ps1
+++ numericVersionNumber.ps1
@@ -1,7 +1,7 @@
 Write-Host "Generating 'versioninfo.h'"
 
 Write-Host "Reading Header..."
-$header = gc "$PSScriptRoot/lua/src/lua.h" | Out-String
+$header = get-content "$PSScriptRoot/lua/src/lua.h" | Out-String
 
 if (($header -match '(?m)^#define\s+LUA_VERSION_MAJOR\s+"(\d+)"\s*$') -ne $true) { throw "Failed to parse LUA_VERSION_MAJOR" }
 $verMajor = $Matches[1]
@@ -12,13 +12,13 @@ $verPatch = $Matches[1]
 
 Write-Host "Parsed version: $verMajor.$verMinor.$verPatch"
 
-$cnt = @"
-#include "lua/src/lua.h"
+$cnt = ([regex]::Matches($header, "^\s*#define\s+LUA[^\n]+", [System.Text.RegularExpressions.RegexOptions]::Multiline) | Select-Object -ExpandProperty Value | Out-String) + @"
+
 #define VER_VERSION $verMajor, $verMinor, $verPatch
 #define VER_VERSION_STR "$verMajor.$verMinor.$verPatch"
 "@
 
 Write-Host "Writing: '$PSScriptRoot/versioninfo.h'"
-sc -Path "$PSScriptRoot/versioninfo.h" -Value $cnt
+set-content -Path "$PSScriptRoot/versioninfo.h" -Value $cnt
 
 Write-Host "Done."
